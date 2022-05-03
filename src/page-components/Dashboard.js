9afb619f002f559a521b402f02768f0d4bc3debf
Nrittik Sarmah <nrittik43@gmail.com>
2022-05-03 10:56:13 +0530

    initial commit

---
+++ src/page-components/Dashboard.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+
+const Dashboard = () => {
+  const [active, setActive] = useState(0);
+  return (
+    <>
+      <header className={styles.header}>
+        <span className={styles.title}>Edvora</span>
+        <div className={styles.profile}>
+          <span>Dhruv Singh</span>
+          <img src="/Nrittik.jpg" alt="" />
+        </div>
+      </header>
+      <div className={styles.body}>
+        <div className={styles.toggler}>
+          <ul>
+            <li
+              className={active === 0 ? styles.active : ""}
+              onClick={() => setActive(0)}
+            >
+              Nearest Rides
+            </li>
+            <li
+              className={active === 1 ? styles.active : ""}
+              onClick={() => setActive(1)}
+            >
+              Upcoming Rides
+            </li>
+            <li
+              className={active === 2 ? styles.active : ""}
+              onClick={() => setActive(2)}
+            >
+              Past Rides
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
